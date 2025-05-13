626339c5ed64cce30301a1d39ba2593e1b9cd262
said yanak <75083985+saidyanak@users.noreply.github.com>
2025-05-13 09:27:11 +0300

    Random a token ile istek atma

--- cargo_app/lib/AuthService/AuthService.dart
+++ cargo_app/lib/AuthService/AuthService.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class AuthService {
-  static const String _url = 'http://localhost:8080/auth/login';  // Login URL
-  static const String _registerUrl = 'http://localhost:8080/auth/register';  // Register URL
+  static const String _baseUrl = 'http://localhost:8080/auth';
+  static const String _randomUrl = 'http://localhost:8080/random';
+  final _secureStorage = FlutterSecureStorage();
 
   Future<String?> login(String username, String password) async {
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse('$_baseUrl/login'),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -16,15 +18,27 @@ class AuthService {
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
-      return data['token'];  // Token'ı döndürüyoruz
+      final String token = data['token'];
+      await _secureStorage.write(key: 'auth_token', value: token);
+      return token;
     } else {
-      return null;  // Hata durumu
+      return null;
+    }
+  }
+
+  Future<void> logout() async {
+    await _secureStorage.delete(key: 'auth_token');
+    final response = await http.post(Uri.parse('$_baseUrl/logout'));
+    if (response.statusCode == 200) {
+      print("Logout başarılı");
+    } else {
+      print("Logout başarısız");
     }
   }
 
   Future<bool> register(String tcOrVkn, String mail, String username, String password, String phoneNumber, String role) async {
     final response = await http.post(
-      Uri.parse(_registerUrl),
+      Uri.parse('$_baseUrl/register'),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -38,23 +52,30 @@ class AuthService {
       }),
     );
 
-    if (response.statusCode == 201) {
-      return true;  // Başarılı register
-    } else {
-      return false;  // Hata durumu
-    }
+    return response.statusCode == 201;
   }
 
-  // Logout işlemi için herhangi bir body gönderilmiyor
-  Future<void> logout() async {
-    final response = await http.post(
-      Uri.parse('http://localhost:8080/auth/logout'),
+  Future<String?> getNameFromToken(String token) async {
+    final String? token = await _secureStorage.read(key: 'auth_token');
+    if (token == null) {
+      print("Token bulunamadı");
+      return null;
+    }
+
+    final response = await http.get(
+      Uri.parse(_randomUrl),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
     );
 
     if (response.statusCode == 200) {
-      print("Logout başarılı");
+      final data = json.decode(response.body);
+      return data['name'];
     } else {
-      print("Logout başarısız");
+      print('Hata: ${response.statusCode}');
+      return null;
     }
   }
 }
